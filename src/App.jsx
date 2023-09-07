0acfe16b9b1146d946dcb5a1fdb137b1a924fe46
Sallar <sallar.ba@outlook.com>
2023-09-07 12:46:22 +0500

    Added Assets, Updated css, Updated Favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
 
   return (
-    <>
+    <div className={styles.App}>
       Hello World!
-    </>
+    </div>
   )
 }
 
